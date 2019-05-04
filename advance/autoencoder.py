4da8e1c0165fe019713212b5143adfbe1ba2c3d3
yanglan <yanglan-17@mails.tsinghua.edu.cn>
2019-05-04 20:30:59 +0800

    autoencoder

--- advance/autoencoder.py
+++ advance/autoencoder.py
@@ -57,3 +57,8 @@ with tf.Session() as sess:
         a[0][i].imshow(np.reshape(mnist.test.images[i],(28,28)))
         a[1][i].imshow(np.reshape(encode_decode[i],(28,28)))
     plt.show()
+
+    encoder_result=sess.run(encoder_op,feed_dict={X:mnist.test.images})
+    plt.scatter(encoder_result[:,0],encoder_result[:,1],c=mnist.test.labels)
+    plt.colorbar()
+    plt.show()
